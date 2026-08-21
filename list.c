660573b903258189ef7d8dd650df3cc2ce0becda
Your Name <you@example.com>
2026-08-21 04:37:28 +0000

    new try

--- list.c
+++ list.c
@@ -165,9 +165,10 @@ void * popCurrent(List * list)
     Node *siguiente = eliminado->next;
 
     if(eliminado->prev != NULL)
-        eliminado->prev->next = eliminado->prev;
+        eliminado->prev->next = eliminado->next;
     else
         list->head = eliminado->next;
+    
     if(eliminado->next != NULL)
         eliminado->next->prev = eliminado->prev;
     else
